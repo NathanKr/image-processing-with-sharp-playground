b9d432b81089c00e8de8a007be06dd6e049451b4
nathankr <natankrasney@gmail.com>
2024-01-31 21:35:09 +0200

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,14 +19,13 @@ export default function Home() {
 
   return (
     <div>
-      <h2>Original</h2>
+      <h2>Original jpg files</h2>
       <div style={{ display: "flex" }}>
         <ImageWithSize filePathRelative={`${LIONS_DIR}/${LION_1280W}.jpg`} />
         <ImageWithSize filePathRelative={`${KITES_DIR}/${KITE_1002W}.jpg`} />
-        <ImageWithSize filePathRelative={`${LION_1024W_WEBP}`} />
       </div>
 
-      <h2>Converted to webp</h2>
+      <h2>Converted jpg files to webp</h2>
       <button onClick={convertToWebp}>Click to convert to webp if images are missing</button>
       <p style={{color:'orange'}}>Notice the size of webp file below is much smaller than jpg above</p>
       <div style={{ display: "flex" }}>
